9b4e3ab6d8fb6dff557eebb61ecde9f26c233965
Steerlean <38030193+ShraddhaAgarwal@users.noreply.github.com>
2018-08-24 14:45:40 +0530

    Update MathFunctionsTest.java

--- src/test/java/MathFunctionsTest.java
+++ src/test/java/MathFunctionsTest.java
@@ -1,9 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 public class MathFunctionsTest {
-    
     MathFunctions m = new MathFunctions();
-
+    
     @Test
     public void addNumbers() {
 
