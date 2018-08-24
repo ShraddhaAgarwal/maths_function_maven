ca3e41c844d8c425d41042697da1416545055626
Shraddha Agarwal <shraddha.agarwal726@gmail.com>
2018-08-24 13:24:29 +0530

    Adding maven project maths function

---
+++ src/main/java/MathFunctions.java
@@ -0,0 +1,16 @@
+public class MathFunctions {
+
+    public int addNumber(int num1,int num2){
+        return num1+num2;
+
+    }
+    public int subtractNumber(int num1,int num2){
+        return num1-num2;
+    }
+    public int multiplyNumber(int num1,int num2){
+        return num1*num2;
+    }
+    public int divideNumber(int num1,int num2){
+        return num1/num2;
+    }
+}
---
+++ src/test/java/MathFunctionsTest.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class MathFunctionsTest {
+    MathFunctions m = new MathFunctions();
+
+    @Test
+    public void addNumbers() {
+
+        Assert.assertEquals(6, m.addNumber(4, 2));
+
+    }
+
+    @Test
+    public void SubtractNumbers() {
+        Assert.assertEquals(2, m.subtractNumber(4, 2));
+
+    }
+
+    @Test
+    public void MultiplyNumbers() {
+
+        Assert.assertEquals(8, m.multiplyNumber(4, 2));
+
+    }
+
+    @Test
+    public void DivideNumbers() {
+        Assert.assertEquals(2, m.divideNumber(4, 2));
+
+
+    }
+}
